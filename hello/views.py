915ad104978777eed86d2072026c7ca956eb43d5
origin <ljhyun715@gmail.com>
2022-01-21 16:44:26 +0900

    python % django tutorial

--- hello/views.py
+++ hello/views.py
@@ -1,21 +1,45 @@
 import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
-def home(request):
-    return HttpResponse("Hello, Django!")
+from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+        
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
 
 def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
 
-    if match_object:
-        clean_name = match_object.group(0)
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
     else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return HttpResponse(content)
+        return render(request, "hello/log_message.html", {"form": form})
